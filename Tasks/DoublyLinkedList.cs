748b427c185e0e946a50979701dafd10a6cb9825
Aliaksei Mukhamiadziyeu <Aliaksei_Mukhamiadziyeu@epam.com>
2022-07-01 18:59:50 +0400

    Implement all methods in DoublyLinkedList (draft, tests green)

--- Tasks/DoublyLinkedList.cs
+++ Tasks/DoublyLinkedList.cs
@@ -5,43 +5,192 @@ using Tasks.DoNotChange;
 
 namespace Tasks
 {
-    public class DoublyLinkedList<T> : IDoublyLinkedList<T>
+    public class DoublyLinkedList<T> : IDoublyLinkedList<T> //TODO remove indices & rethink how they should operate
     {
-        public int Length => throw new NotImplementedException();
+        private Node _rootNode;
+
+        public int Length
+        {
+            get
+            {
+                if (_rootNode == null)
+                {
+                    return 0;
+                }
+
+                var len = 1; // some weird shit going on here
+                var current = _rootNode;
+                while (current.next != null)
+                {
+                    len++;
+                    current = current.next;
+                }
+                return len;
+            }
+        }
 
         public void Add(T e)
         {
-            throw new NotImplementedException();
+            if (_rootNode == null)
+            {
+                _rootNode = new Node(e, Length);
+                return;
+            }
+
+            Node current = _rootNode;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = new Node(e, Length);
+            current.next.prev = current;
         }
 
         public void AddAt(int index, T e)
         {
-            throw new NotImplementedException();
+            if (_rootNode == null)
+            {
+                _rootNode = new Node(e, Length);
+                return;
+            }
+
+            Node current = _rootNode;
+            while (current.next != null && current.index < index)
+            {
+                current = current.next;
+            }
+
+            if (current.index == Length)
+            {
+                current.next = new Node(current.data, Length);
+                current.next.prev = current;
+                current.data = e;
+                return;
+            }
+
+            var newNode = new Node(e, index);
+
+            if (current.prev != null)
+            {
+                newNode.next = current;
+                newNode.prev = current.prev;
+                newNode.prev.next = newNode;
+                current.prev = newNode;
+                current.index++;
+                return;
+            }
+            
+            _rootNode = newNode;
+            newNode.next = current;
+            newNode.prev = current.prev;
+            current.prev = newNode;
+            current.index++;
         }
 
         public T ElementAt(int index)
         {
-            throw new NotImplementedException();
+            if (_rootNode == null || index >= Length || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Node current = _rootNode;
+            while (current.index < index && current.next != null)
+            {
+                current = current.next;
+            }
+
+            return current.data;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new DoublyLinkedListEnum<T>(this);
         }
 
         public void Remove(T item)
         {
-            throw new NotImplementedException();
+            Node current = _rootNode;
+            do
+            {
+                if (current.data.Equals(item) && current.prev == null)
+                {
+                    current.next.prev = current.prev;
+                    _rootNode = current.next;
+                    _rootNode.index--;
+                    return;
+                }
+
+                if (current.data.Equals(item) && current.next == null)
+                {
+                    current.prev.next = current.next;
+                    return;
+                }
+
+                if (current.data.Equals(item) && current.next != null && current.prev == null)
+                {
+                    current.next.prev = current.prev;
+                    current.prev.next = current.next;
+                    return;
+                }
+
+                current = current.next;
+            } while (current != null);
         }
 
         public T RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (_rootNode == null || index >= Length || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Node current = _rootNode;
+            while (current.index <= index)
+            {
+                if (current.index == index && current.prev == null)
+                {
+                    current.next.prev = current.prev;
+                    _rootNode = current.next;
+                    _rootNode.index--;
+                    return current.data;
+                }
+
+                if (current.index == index && current.next == null)
+                {
+                    current.prev.next = current.next;
+                    return current.data;
+                }
+
+                if (current.index == index)
+                {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                    return current.data;
+                }
+                current = current.next;
+            }
+
+            return default;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+
+        public class Node
+        {
+            public T data;
+            public Node prev;
+            public Node next;
+            public int index;
+
+            public Node(T value, int index)
+            {
+                data = value;
+                this.index = index;
+            }
         }
     }
 }
---
+++ Tasks/DoublyLinkedListEnum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Tasks
+{
+    public class DoublyLinkedListEnum<T> : IEnumerator<T>
+    {
+        public DoublyLinkedList<T> RootNode;
+
+        int _position = -1;
+
+        public DoublyLinkedListEnum(DoublyLinkedList<T> rootNode)
+        {
+            RootNode = rootNode;
+        }
+
+        public bool MoveNext()
+        {
+            _position++;
+            return _position < RootNode.Length;
+        }
+
+        public void Reset()
+        {
+            _position = -1;
+        }
+
+        public T Current
+        {
+            get
+            {
+                try
+                {
+                    return RootNode.ElementAt(_position);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+    }
+}
