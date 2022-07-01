0591a6740387155086a6ac09b1e5b41266b395d5
Aliaksei Mukhamiadziyeu <Aliaksei_Mukhamiadziyeu@epam.com>
2022-07-01 18:59:50 +0400

    Implement Length, Add, AddAt, ElementAt methods

--- Tasks/DoublyLinkedList.cs
+++ Tasks/DoublyLinkedList.cs
@@ -7,21 +7,99 @@ namespace Tasks
 {
     public class DoublyLinkedList<T> : IDoublyLinkedList<T>
     {
-        public int Length => throw new NotImplementedException();
+        private Node _rootNode;
+        public int Length
+        {
+            get
+            {
+                if (_rootNode == null)
+                {
+                    return 0;
+                }
+
+                var len = 1;
+                var current = _rootNode;
+                while (current.next != null)
+                {
+                    len++;
+                    current = current.next;
+                }
+                return len;
+            }
+        }
 
         public void Add(T e)
         {
-            throw new NotImplementedException();
+            if (_rootNode == null)
+            {
+                _rootNode = new Node(e, Length);
+                return;
+            }
+
+            Node current = _rootNode;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = new Node(e, Length);
+            current.next.prev = current;
         }
 
         public void AddAt(int index, T e)
         {
-            throw new NotImplementedException();
+            if (_rootNode == null)
+            {
+                _rootNode = new Node(e, Length);
+                return;
+            }
+
+            Node current = _rootNode;
+            while (current.next != null && current.index < index)
+            {
+                current = current.next;
+            }
+
+            if (current.index == Length)
+            {
+                current.next = new Node(current.data, Length);
+                current.next.prev = current;
+                current.data = e;
+                return;
+            }
+
+            var newNode = new Node(e, index);
+
+            if (current.prev != null)
+            {
+                newNode.next = current;
+                newNode.prev = current.prev;
+                newNode.prev.next = newNode;
+                current.prev = newNode;
+                current.index++;
+                return;
+            }
+            
+            _rootNode = newNode;
+            newNode.next = current;
+            newNode.prev = current.prev;
+            current.prev = newNode;
+            current.index++;
         }
 
         public T ElementAt(int index)
         {
-            throw new NotImplementedException();
+            if (_rootNode == null || index >= Length || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Node current = _rootNode;
+            while (current.index < index && current.next != null)
+            {
+                current = current.next;
+            }
+
+            return current.data;
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -41,7 +119,21 @@ namespace Tasks
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+
+        public class Node
+        {
+            public T data;
+            public Node prev;
+            public Node next;
+            public int index;
+
+            public Node(T value, int index)
+            {
+                data = value;
+                this.index = index;
+            }
         }
     }
 }
